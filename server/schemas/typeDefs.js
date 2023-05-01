06fb552d697825915649bee06d6b3edc9eea4f91
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-01 20:07:07 +1000

    add typeDefs to schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+
+    type Query {
+        me: User
+    }
+
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+      }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
+`;
+
+module.exports = typeDefs;
