f684846b3cfba9713422f9cd0acc87d08a810803
Valeriya <valeriya.finogeeva@gmail.com>
2023-05-01 16:29:41 +1000

    Update front-end and remove unused components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, FindUs, Footer, Gallery, Header, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
@@ -10,9 +10,6 @@ const App = () => (
     <Header />
     <AboutUs />
     <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
     <Gallery />
     <FindUs />
     <Footer />
