8450303d243f38979dbd234b3cef4aeef1972315
Valeriya <valeriya.finogeeva@gmail.com>
2023-05-02 20:24:25 +1000

    Add login/signup functionality to the app

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 // import { createUser } from '../utils/API';
-import { ADD_USER } from "../utils/mutations";
+import { ADD_USER } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
-import Auth from '../utils/auth';
+import Auth from '../../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
