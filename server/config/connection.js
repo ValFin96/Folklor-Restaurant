07d04cc3f07e45cb7567e44bc416f7c2d1119b64
Valeriya <valeriya.finogeeva@gmail.com>
2023-05-08 20:23:53 +1000

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/folklor-restaurant');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Folklor');
 
 module.exports = mongoose.connection;
